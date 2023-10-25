8387034bbb84905a3d0c3612065ac873cf015543
Nick Colvin <njc@alumni.unc.edu>
2023-10-25 16:58:28 -0500

    fixed using 1 RV, but slightly less accurate

--- QueueingSystem.java
+++ QueueingSystem.java
@@ -4,12 +4,21 @@ class QueueingSystem {
     
     // K = capacity of system, m = number of servers, N = number of customers in system
     int K, m, N;
-    // mu = service rate of a server
-    // gamma = arrival rate of first machine
-    // rho = utilization of all servers
+    // µ = service rate of a server
+    // γ = arrival rate of first machine
+    // ρ = utilization of all servers
     float mu, gamma = 5, rho, clock;
     ArrayList<Event> elist;
     boolean done;
+    float getArrivalRate() {
+        // λ = computed arrival rate of second machine
+        float effective_rate = this.rho * this.m * this.mu;
+        if (this.N < 2) // effective arrival rate = λ + γ (superposition)
+            effective_rate += this.gamma;
+        return effective_rate;
+    }
+    // set when N >= 2, remove and replace with combined rate when N = 2 and departure
+    Event lambdaArr;
 
     QueueingSystem(int K, int m, float mu, float rho) {
         this.K = K;
@@ -29,20 +38,12 @@ class QueueingSystem {
         this.elist.add(i, e);
     }
 
-    float getLambda() {
-        // lambda = computed arrival rate of second machine
-        float effective_rate = this.rho * this.m * this.mu;
-        if (this.N < 2) // effective arrival rate = lambda + gamma (superposition)
-            effective_rate += this.gamma;
-        return effective_rate;
-    }
-
     void run() {
-        int num_dep = 0;
+        int num_dep = 0, num_arr = 0;
         // area = area under the graph of number of customers in system vs time
         float area = 0;
         // insert first arrival
-        this.insertEvent(new Event(EventType.ARR, Exponential.get(this.getLambda())));
+        this.insertEvent(new Event(EventType.ARR, Exponential.get(this.getArrivalRate())));
         while (!this.done) {
             // pop off the event list and update clock
             Event currEvent = this.elist.remove(0);
@@ -53,11 +54,15 @@ class QueueingSystem {
                     if (this.N == this.K) {
                         // discard i.e. block
                         // generate next arrival
-                        this.insertEvent(new Event(EventType.ARR, this.clock + Exponential.get(this.getLambda())));
+                        this.insertEvent(new Event(EventType.ARR, this.clock + Exponential.get(this.getArrivalRate())));
                     } else {
                         this.N++;
+                        num_arr++;
                         // generate next arrival
-                        this.insertEvent(new Event(EventType.ARR, this.clock + Exponential.get(this.getLambda())));
+                        Event nextArr = new Event(EventType.ARR, this.clock + Exponential.get(this.getArrivalRate()));
+                        if (this.N >= 2) // mark
+                            this.lambdaArr = nextArr;
+                        this.insertEvent(nextArr);
                         if (this.N <= this.m) // service
                             this.insertEvent(new Event(EventType.DEP, this.clock + Exponential.get(this.mu)));
                     }
@@ -65,6 +70,11 @@ class QueueingSystem {
                 case DEP:
                     num_dep++;
                     this.N--;
+                    if (this.N == 1 && this.elist.contains(this.lambdaArr)) { // replace λ arrival with λ + γ
+                        this.elist.remove(this.lambdaArr);
+                        this.lambdaArr = null;
+                        this.insertEvent(new Event(EventType.ARR, this.clock + Exponential.get(this.getArrivalRate())));
+                    }
                     if (this.N >= this.m) // service next customer
                         this.insertEvent(new Event(EventType.DEP, this.clock + Exponential.get(this.mu)));
                     break;
@@ -72,20 +82,25 @@ class QueueingSystem {
             if (num_dep > 100000)
                 this.done = true;
         }
-        System.out.println(area / this.clock);
+        System.out.println("ρ = " + this.rho);
+        // E[n] = area / t_end
+        System.out.println(" E[n] = " + area / this.clock);
+        // E[𝜏] = area / total # arrs
+        System.out.println(" E[𝜏] = " + area / num_arr);
+        System.out.println();
     }
 
     public static void main(String[] args) {
         if (args.length != 3)
-            throw new IllegalArgumentException("usage: java QueueingSystem K m mu rho");
+            throw new IllegalArgumentException("usage: java QueueingSystem K m µ");
         
         int K = Integer.parseInt(args[0]);
         int m = Integer.parseInt(args[1]);
         float mu = Float.parseFloat(args[2]);
-        float rho = 0;
+        float rho;
         QueueingSystem sys;
         for (int i = 0; i < 10; i++) {
-            rho += 0.1;
+            rho = (i + 1) / 10f;
             sys = new QueueingSystem(K, m, mu, rho);
             sys.run();
         }
